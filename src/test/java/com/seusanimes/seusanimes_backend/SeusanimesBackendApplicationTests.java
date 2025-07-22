ae6085d29f45cf48e96454053336c6c14cf5928f
adriel morais <adrie3lmorais@gmail.com>
2025-07-22 17:16:22 -0300

    Primeiro commit: Estrutura inicial do backend Spring Boot com segurança JWT

---
+++ src/test/java/com/seusanimes/seusanimes_backend/SeusanimesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seusanimes.seusanimes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeusanimesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
